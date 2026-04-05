0b4d59a966bd47b34f904212e85a2ebe0cd6cc82
AhDuxng <dungpham.coding@gmail.com>
2026-04-05 23:52:03 +0700

    Add '-insecure' option to ssh3 command in LatencyBenchmarker

--- W1Benchmark.py
+++ W1Benchmark.py
@@ -25,7 +25,7 @@ class LatencyBenchmarker:
         if protocol == "ssh":
             full_cmd = ["ssh", "-o", "BatchMode=yes", self.target, command]
         elif protocol == "ssh3":
-            full_cmd = ["ssh3", self.target, command]
+            full_cmd = ["ssh3", "-insecure", self.target, command]
         elif protocol == "mosh":
             full_cmd = ["mosh", "--ssh=ssh -o BatchMode=yes", self.target, "--", command]
 
