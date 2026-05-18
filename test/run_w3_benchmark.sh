2de81ea05b0bc991e04d3dfaa2c18542653a5e90
AhDuxng <dungpham.coding@gmail.com>
2026-05-18 16:50:58 +0700

    feat: update protocols to include ssh and ssh3, increase trials to three for improved benchmarking

--- test/run_w3_benchmark.sh
+++ test/run_w3_benchmark.sh
@@ -6,12 +6,12 @@ USER_NAME="trungnt"
 SOURCE_IP="192.168.8.100"
 IDENTITY_FILE="$HOME/.ssh/id_rsa" 
 
-PROTOCOLS="mosh"                          
+PROTOCOLS="ssh ssh3 mosh"                          
 WORKLOADS="interactive_shell vim nano"   
 
 ITERATIONS=100         
 WARMUP_ROUNDS=10       
-TRIALS=1           
+TRIALS=3           
 TIMEOUT=20           
 SEED=42                
 
